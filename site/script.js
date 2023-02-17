c980b745a12161a14e47738c1e0d8b72fc00310d
zmm29 <andre.all3@hotmail.com>
2023-02-17 01:00:55 -0300

    atualizando

--- site/script.js
+++ site/script.js
@@ -1,18 +1,17 @@
 function carregar() {
-    var msg = window.document.getElementsById(`msg`)
-    var img = window.document.getElementsById(`imagem`)
+    var msg = window.document.getElementById(`msg`)
+    var img = window.document.getElementById(`imagem`)
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 20
-    msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >= 0 && hora < 12) {
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 6 && hora < 12) {
         //bomdia
         img.src = `manha.jpg`
-        document.body.style.background = 'red'
+        document.body.style.background = 'yellow'
     } else if (hora >= 12 && hora <= 18) {
         //boa tarde
         img.src = `tarde.jpg`
-        document.body.style.background = 'blue'
+        document.body.style.background = 'orange'
     } else {
         //boa noite
         img.src = `noite.jpg`
