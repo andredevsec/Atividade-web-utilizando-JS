6687c2062d8f77774625eb885814b15aa4ee8fbb
zmm29 <andre.all3@hotmail.com>
2023-02-22 16:09:03 -0300

    Ajute final

--- site/script.js
+++ site/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById(`msg`)
     var img = window.document.getElementById(`imagem`)
     var data = new Date()
-    var hora = data.getHours()
+    var hora = data.getHours()  
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 6 && hora < 12) {
         //bomdia
